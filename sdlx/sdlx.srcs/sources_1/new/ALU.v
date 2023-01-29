a10068dc1869a4efab234d5063f5a03663ff375e
Tirth Patel <tirth1093@gmail.com>
2023-01-29 15:12:11 +0530

    initial commit

---
+++ sdlx/sdlx.srcs/sources_1/new/ALU.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/29/2023 03:07:30 PM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+
+    );
+endmodule
